ba2ba1253687d4035490298831be73df01dcb129
Minusten <chancyfancy23@gmail.com>
2024-11-24 00:07:49 +0530

    refactor: clean up code & imports

--- src/main/java/dev/projectorigin/xcore/Xcore.java
+++ src/main/java/dev/projectorigin/xcore/Xcore.java
@@ -1,97 +1,21 @@
 package dev.projectorigin.xcore;
 
-import com.comphenix.protocol.PacketType;
-import com.comphenix.protocol.ProtocolLibrary;
-import com.comphenix.protocol.events.ListenerPriority;
-import dev.projectorigin.xcore.hud.TFMPacketListener;
-import dev.projectorigin.xcore.hud.TextProcessor;
 import dev.projectorigin.xcore.items.ItemGiveCommand;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.HandlerList;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Xcore extends JavaPlugin {
+import java.util.Objects;
 
-    private TextProcessor textProcessor;
+public final class Xcore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        migrateConfiguration();
-        setupConfiguration();
-        textProcessor = new TextProcessor(this);
-        ProtocolLibrary.getProtocolManager().addPacketListener(new TFMPacketListener(
-                this, ListenerPriority.NORMAL,
-                PacketType.Play.Server.SET_ACTION_BAR_TEXT,
-                PacketType.Play.Server.BOSS,
-                PacketType.Play.Server.SCOREBOARD_OBJECTIVE,
-                PacketType.Play.Server.SCOREBOARD_TEAM));
-
-        // Register the "giveitem" command
-        getCommand("giveitem").setExecutor(new ItemGiveCommand());
-        getCommand("giveitem").setTabCompleter(new ItemGiveCommand());
-
+        Objects.requireNonNull(getCommand("giveitem")).setExecutor(new ItemGiveCommand());
+        Objects.requireNonNull(getCommand("giveitem")).setTabCompleter(new ItemGiveCommand());
     }
 
     @Override
     public void onDisable() {
         HandlerList.unregisterAll(this);
-    }
-
-    public TextProcessor getTextProcessor() {
-        return textProcessor;
-    }
-
-    // the following code is used for ACTIONBAR
-    private void noKeyFound(String ...keys) {
-        getLogger().warning("Configuration migration: Couldn't find configuration key(s) %s, skipping...".formatted(String.join(", ", keys)));
-    }
-
-    private void migrateConfiguration() {
-        getLogger().info("Checking for configuration migration...");
-        var config = getConfig();
-        if (config.getKeys(false).isEmpty()) {
-            getLogger().info("Empty configuration, creating...");
-            return;
-        }
-        if (config.isInt("config-version")) {
-            getLogger().info("Configuration key config-version exists, assuming there's no need for migration :)");
-            return;
-        }
-
-        if (config.isString("font") && config.isString("special-symbol-for-scoreboards")) {
-            config.set("fonts.default-font.name", config.get("font"));
-            config.set("fonts.default-font.special-symbol", config.get("special-symbol-for-scoreboards"));
-        } else noKeyFound("font", "special-symbol-for-scoreboards");
-        var regex = config.get("regex");
-        config.set("regex", null);
-        if (regex != null && config.isBoolean("invert-regex")) {
-            config.set("regex.value", regex);
-            config.set("regex.invert", config.get("invert-regex"));
-        } else noKeyFound("font", "invert-regex");
-        config.set("packets", null);
-        config.set("invert-regex", null);
-        config.set("font", null);
-        config.set("special-symbol-for-scoreboards", null);
-        config.set("config-version", 1);
-        getLogger().info("Successfully migrated to a new configuration!");
-        saveConfig();
-    }
-
-    private void setupConfiguration() {
-        var config = getConfig();
-        if (!config.isConfigurationSection("fonts")) {
-            config.addDefault("fonts.default-font.name", "namespace:key");
-            config.addDefault("fonts.default-font.special-symbol", "$u");
-        }
-        config.addDefault("regex.value", "[\\p{Print}&&[^~,],]+");
-        config.addDefault("regex.invert", false);
-        for (var key : List.of("boss-bar", "action-bar", "scoreboard-title", "scoreboard-scores")) {
-            config.addDefault("packets.%s.enable".formatted(key), true);
-            config.addDefault("packets.%s.forced-font".formatted(key), key.equals("scoreboard-scores") ? "" : "default-font");
-        }
-        config.addDefault("config-version", 1);
-        getConfig().options().copyDefaults(true);
-        saveConfig();
     }
 }
--- src/main/java/dev/projectorigin/xcore/items/ItemGiveCommand.java
+++ src/main/java/dev/projectorigin/xcore/items/ItemGiveCommand.java
@@ -1,49 +1,45 @@
 package dev.projectorigin.xcore.items;
+
+import net.kyori.adventure.text.Component;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.attribute.AttributeModifier;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.inventory.EquipmentSlot;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.NamespacedKey;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.persistence.PersistentDataContainer;
-import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 public class ItemGiveCommand implements CommandExecutor, TabCompleter {
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
 
         if (args[0].equalsIgnoreCase("nocturnite")) {
-            ItemStack thisitem = new ItemStack(Material.PAPER); {
-                ItemMeta meta = thisitem.getItemMeta();
+            ItemStack nocturnite = new ItemStack(Material.PAPER); {
+                ItemMeta meta = nocturnite.getItemMeta();
                 meta.setCustomModelData(4000);
-                meta.setItemName("§fNocturnite");
-                thisitem.setItemMeta(meta);
-                player.getInventory().addItem(thisitem);
+                meta.itemName(Component.text("Nocturnite"));
+                nocturnite.setItemMeta(meta);
+                player.getInventory().addItem(nocturnite);
                 return true;
             }
-        }
-        if (args[0].equalsIgnoreCase("nocturnite_axe")) {
-            ItemStack thisitem = new ItemStack(Material.NETHERITE_AXE); {
-                ItemMeta meta = thisitem.getItemMeta();
+        } else if (args[0].equalsIgnoreCase("nocturnite_axe")) {
+            ItemStack nocturniteAxe = new ItemStack(Material.NETHERITE_AXE);
+            {
+                ItemMeta meta = nocturniteAxe.getItemMeta();
                 meta.setCustomModelData(1);
-                meta.setItemName("§fNocturnite Axe");
+                meta.itemName(Component.text("Nocturnite Axe"));
                 AttributeModifier damageModifier = new AttributeModifier(
                         UUID.randomUUID(), // Unique ID
                         "generic.attackDamage", // Attribute to modify
@@ -52,42 +48,28 @@ public class ItemGiveCommand implements CommandExecutor, TabCompleter {
                         EquipmentSlot.HAND // Apply only when in main hand
                 );
                 meta.addAttributeModifier(Attribute.GENERIC_ATTACK_DAMAGE, damageModifier);
-                thisitem.setItemMeta(meta);
-                player.getInventory().addItem(thisitem);
+                nocturniteAxe.setItemMeta(meta);
+                player.getInventory().addItem(nocturniteAxe);
                 return true;
             }
-        }
-        if (args[0].equalsIgnoreCase("nocturnite_hoe")) {
-            ItemStack thisitem = new ItemStack(Material.NETHERITE_HOE); {
-                ItemMeta meta = thisitem.getItemMeta();
+        } else if (args[0].equalsIgnoreCase("nocturnite_hoe")) {
+            ItemStack nocturniteHoe = new ItemStack(Material.NETHERITE_HOE); {
+                ItemMeta meta = nocturniteHoe.getItemMeta();
                 meta.setCustomModelData(1);
-                meta.setItemName("§fNocturnite Hoe");
-                thisitem.setItemMeta(meta);
-                player.getInventory().addItem(thisitem);
+                meta.itemName(Component.text("Nocturnite Hoe"));
+                nocturniteHoe.setItemMeta(meta);
+                player.getInventory().addItem(nocturniteHoe);
                 return true;
             }
         }
 
         return false;
     }
+
     @Nullable
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        List<String> suggestions = new ArrayList<>();
-
-        if (args.length == 1) {
-            // Provide a list of items for tab completion
-            List<String> items = Arrays.asList(
-                    "nocturnite","nocturnite_axe","nocturnite_hoe"
-
-            );
-            for (String item : items) {
-                if (item.toLowerCase().startsWith(args[0].toLowerCase())) {
-                    suggestions.add(item);
-                }
-            }
-        }
-
-        return suggestions;
+        return Arrays.asList("nocturnite", "nocturnite_axe", "nocturnite_hoe");
     }
+
 }
